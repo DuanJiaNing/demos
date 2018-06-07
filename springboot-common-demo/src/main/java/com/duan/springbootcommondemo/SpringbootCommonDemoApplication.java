e95e1bcabf31b91365f82227d59998563f9c97d4
DuanJiaNing <duanjianing@17m.club>
2018-06-07 20:33:14 +0800

    logback 日志文件

---
+++ springboot-common-demo/src/main/java/com/duan/springbootcommondemo/SpringbootCommonDemoApplication.java
@@ -0,0 +1,12 @@
+package com.duan.springbootcommondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCommonDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCommonDemoApplication.class, args);
+    }
+}
