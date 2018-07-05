c33275a3e4c3cfa4ce74cbff5f9f5f350a383ed7
DuanJiaNing <aimeimeits@gmail.com>
2018-07-05 08:41:34 +0800

    添加 netty-demo

---
+++ netty-demo/src/main/java/com/duan/nettydemo/NettyDemoApplication.java
@@ -0,0 +1,12 @@
+package com.duan.nettydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyDemoApplication.class, args);
+    }
+}
