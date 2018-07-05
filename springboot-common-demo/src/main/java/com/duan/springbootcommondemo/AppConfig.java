7fcfcf29d964f104b3dcc80744269e117a14f082
DuanJiaNing <duanjianing@17m.club>
2018-07-05 13:13:13 +0800

    spring boot

---
+++ springboot-common-demo/src/main/java/com/duan/springbootcommondemo/AppConfig.java
@@ -0,0 +1,12 @@
+package com.duan.springbootcommondemo;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created on 2018/7/5.
+ *
+ * @author DuanJiaNing
+ */
+@Configuration
+public class AppConfig {
+}
---
+++ springboot-common-demo/src/main/java/com/duan/springbootcommondemo/SpringBootCommonDemoApplication.java
@@ -0,0 +1,18 @@
+package com.duan.springbootcommondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created on 2018/7/5.
+ *
+ * @author DuanJiaNing
+ */
+@SpringBootApplication
+public class SpringBootCommonDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCommonDemoApplication.class, args);
+    }
+
+}
