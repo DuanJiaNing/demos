5a40e92874d88d1f38f01da342711e75c9b1a7a4
DuanJiaNing <aimeimeits@gmail.com>
2018-08-30 08:42:18 +0800

    spring security

--- spring-mvc-demo/src/java/com/duan/springmvcdemo/app/SecurityConfig.java
+++ spring-mvc-demo/src/java/com/duan/springmvcdemo/app/SecurityConfig.java
@@ -1,11 +1,19 @@
 package com.duan.springmvcdemo.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created on 2018/8/28.
@@ -16,10 +24,41 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+
+        // 内存认证
+        auth.inMemoryAuthentication()
+                .withUser("user").password("abc").roles("USER").and()
+                .withUser("admin").password("abc").roles("USER", "ADMIN");
+
+
+        // jdbc 数据库认证
+        String idForEncode = "pwdEncode_1";
+        Map<String, PasswordEncoder> map = new HashMap<>();
+        map.put(idForEncode, new BCryptPasswordEncoder(10));
+
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("select username,password,true from spitter where username=?")
+                .authoritiesByUsernameQuery("select username, 'ROLE_USER' from spitter where username=?")
+                .passwordEncoder(new DelegatingPasswordEncoder(idForEncode, map))
+        /*.groupAuthoritiesByUsername()*/;
+
+
+        // ldap 认证
+        auth.ldapAuthentication()
+                .userSearchBase("ou=people")
+                .userSearchFilter("(uid={0})")
+                .groupSearchBase("ou=groups")
+                .groupSearchFilter("member={0}")
+                .contextSource().url("ldap://habuma.com:389/dc=habuma,dc=com").and()
+                .passwordCompare()
+                .passwordEncoder(new DelegatingPasswordEncoder(idForEncode, map))
+                .passwordAttribute("password");
+
     }
 
     @Override // 配置 filter 链
@@ -31,4 +70,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
     }
+
+
 }
