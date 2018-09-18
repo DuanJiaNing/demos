f886a7dc92df217a395d14066fbdd79c2f399733
DuanJiaNing <aimeimeits@gmail.com>
2018-09-18 08:43:04 +0800

    redis cache 缓存技术

--- springboot-common-demo/src/main/java/com/duan/springbootcommondemo/config/CacheConfig.java
+++ springboot-common-demo/src/main/java/com/duan/springbootcommondemo/config/CacheConfig.java
@@ -9,7 +9,75 @@ import org.springframework.context.annotation.Configuration;
  * @author DuanJiaNing
  */
 @Configuration
-@EnableCaching
+@EnableCaching // 使用 redis 时 spring boot 默认配置了 JedisConnectionFactory RedisTemplate StringRedisTemplate
 public class CacheConfig {
+/*
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+
+    @Value("${spring.redis.pool.maxIdle}")
+    private int maxIdle;
+
+    @Value("${spring.redis.pool.maxTotal}")
+    private int maxTotal;
+
+    @Value("${spring.redis.pool.maxWait}")
+    private int maxWait;
+
+    @Value("${spring.redis.pool.testOnBorrow}")
+    private boolean testOnBorrow;
+
+    @Value("${spring.redis.pool.testOnReturn}")
+    private boolean testOnReturn;
+
+    @Bean
+    public JedisPoolConfig jedisPoolConfig() {
+
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxIdle(maxIdle);
+        jedisPoolConfig.setMaxTotal(maxTotal);
+        jedisPoolConfig.setMaxWaitMillis(maxWait);
+        jedisPoolConfig.setTestOnBorrow(testOnBorrow);
+        jedisPoolConfig.setTestOnReturn(testOnReturn);
+
+        return jedisPoolConfig;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+
+        jedisConnectionFactory.setHostName(host);
+        jedisConnectionFactory.setPort(port);
+        jedisConnectionFactory.setPassword(password);
+        jedisConnectionFactory.setPoolConfig(jedisPoolConfig());
+
+        return jedisConnectionFactory;
+    }
+
+    @Bean
+    RedisTemplate redisTemplate() {
+
+        final RedisTemplate<String, Long> template = new RedisTemplate();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new JdkSerializationRedisSerializer());
+        return template;
+
+    }
+*/
 
 }
